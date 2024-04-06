84473e6ea1b73679412c7ae9ec8fe8bb045e36ff
Leo Puvilland <leo@craftcat.dev>
2024-04-06 14:47:13 -0700

    add cachix.nix to default.nix

--- hosts/blackeye/default.nix
+++ hosts/blackeye/default.nix
@@ -7,9 +7,15 @@
 }:
 {
   imports = [
+
+    # Server Setup
     ./hardware.nix
     ./secrets.nix
     ./networking.nix
+
+
+    # Services
+    ./services/misc/cachix.nix
   ];
 
   system.stateVersion = "23.11"; # DO NOT EDIT
