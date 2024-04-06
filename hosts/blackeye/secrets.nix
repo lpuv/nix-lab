ab922a961a51b4b534e9b54fa80deaeec7523091
Leo Puvilland <leo@craftcat.dev>
2024-04-05 22:40:29 -0700

    add age identity paths

--- hosts/blackeye/secrets.nix
+++ hosts/blackeye/secrets.nix
@@ -1,5 +1,16 @@
-{ ... }:
+{ config, lib, ... }:
 {
+  age.identityPaths =
+    [
+      # make sure this key is copied from 1password prior to running agenix
+      "/home/leo/.ssh/id_ed25519"
+      # key to use for new installs, prior to generation of hostKeys
+      "/etc/agenixKey"
+    ]
+    ++ map (e: e.path) (
+      lib.filter (e: e.type == "rsa" || e.type == "ed25519") config.services.openssh.hostKeys
+    );
+
   age.secrets.cachix-token = {
     file = ../../secrets/cachix.age;
   };
