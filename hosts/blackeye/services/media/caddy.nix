deb962513063f424790b38afac68c3cd075f706c
Leo Puvilland <leo@craftcat.dev>
2024-04-08 10:23:19 -0700

    copy package derivation from PR

--- hosts/blackeye/services/media/caddy.nix
+++ hosts/blackeye/services/media/caddy.nix
@@ -3,7 +3,7 @@
   services.caddy = {
     enable = true;
     # Custom package to allow for plugins, once https://github.com/NixOS/nixpkgs/pull/259275 is merged, delete this!
-    package = (pkgs.callPackage "${builtins.fetchurl { url = "https://raw.githubusercontent.com/jpds/nixpkgs/a33b02fa9d664f31dadc8a874eb1a5dbaa9f4ecf/pkgs/servers/caddy/default.nix"; sha256 = "sha256-rmzG/Wbt0T5lF7aElgu/pPcRs5au2sK8FJ+yCj02L/Q="; }}" {
+    package = (pkgs.callPackage ../../../../pkgs/caddy.nix {
       externalPlugins = [
         { name = "caddy-dns/cloudflare"; repo = "github.com/caddy-dns/cloudflare"; version = "44030f9306f4815aceed3b042c7f3d2c2b110c97"; }
       ];
