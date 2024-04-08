c8f4f873c6953bd7d3e5d4f5bb9ca256ef4c4254
Leo Puvilland <leo@craftcat.dev>
2024-04-08 10:04:27 -0700

    fix sha256sum

--- hosts/blackeye/services/media/caddy.nix
+++ hosts/blackeye/services/media/caddy.nix
@@ -3,7 +3,7 @@
   services.caddy = {
     enable = true;
     # Custom package to allow for plugins, once https://github.com/NixOS/nixpkgs/pull/259275 is merged, delete this!
-    package = (pkgs.callPackage "${builtins.fetchurl { url = "https://raw.githubusercontent.com/jpds/nixpkgs/a33b02fa9d664f31dadc8a874eb1a5dbaa9f4ecf/pkgs/servers/caddy/default.nix"; sha256 = "ae6cc6fd66edd13e6517b684960bbfa4f711b396aedac2bc149fb20a3d362ff4"; }}" {
+    package = (pkgs.callPackage "${builtins.fetchurl { url = "https://raw.githubusercontent.com/jpds/nixpkgs/a33b02fa9d664f31dadc8a874eb1a5dbaa9f4ecf/pkgs/servers/caddy/default.nix"; sha256 = "sha256-C7JOGd4sXsRZL561oP84V2/pTg7szEgF4OFOw35yS1s="; }}" {
       externalPlugins = [
         { name = "caddy-dns/cloudflare"; repo = "github.com/caddy-dns/cloudflare"; version = "44030f9306f4815aceed3b042c7f3d2c2b110c97"; }
       ];
