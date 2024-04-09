79c29d496dc62b4b2afb629717244bc55f43cf5a
Leo Puvilland <leo@craftcat.dev>
2024-04-08 19:21:00 -0700

    add sonarr

---
+++ hosts/blackeye/services/media/sonarr.nix
@@ -0,0 +1,25 @@
+{ pkgs, config, ... }:
+{
+  services.sonarr = {
+    enable = true;
+    group = "media";
+    package = pkgs.unstable.sonarr;
+  };
+
+  services.caddy.virtualHosts."sonarr.media.internal.craftcat.dev" = {
+    extraConfig = ''
+      import ${config.age.secrets.caddy.path}
+      reverse_proxy http://localhost:8989
+    '';
+  };
+
+  # Ensure that sonarr waits for the downloads and media directories to be
+  # available.
+  systemd.services.sonarr = {
+    wantedBy = [ "multi-user.target" ];
+    after = [
+      "network.target"
+      "mnt-media.automount"
+    ];
+  };
+}
