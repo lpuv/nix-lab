64c3ec3d63abc0f42521fe83844ca9f1e84c32c2
Leo Puvilland <leo@craftcat.dev>
2024-04-08 18:28:30 -0700

    add config attribute

--- hosts/blackeye/services/media/transmission.nix
+++ hosts/blackeye/services/media/transmission.nix
@@ -42,7 +42,7 @@
      };
    };
 
-  services.caddy.virtualHosts."dl.internal.craftcat.dev" = {
+  config.services.caddy.virtualHosts."dl.internal.craftcat.dev" = {
     extraConfig = ''
       reverse_proxy http://127.0.0.1:9091
       import ${config.age.secrets.caddy.path}
