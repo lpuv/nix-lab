642a4beddaebc3522f2e905553f68532084695f8
Leo Puvilland <leo@craftcat.dev>
2024-04-09 13:28:50 -0700

    change the execstart for prowlarr

--- hosts/blackeye/services/media/prowlarr.nix
+++ hosts/blackeye/services/media/prowlarr.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
   services.prowlarr = {
     enable = true;
@@ -9,5 +9,12 @@
       import ${config.age.secrets.caddy.path}
       reverse_proxy http://localhost:9696
     '';
+  };
+
+  # if https://github.com/NixOS/nixpkgs/pull/302919 gets merged, will not need this anymore
+  systemd.services.prowlarr = {
+    serviceConfig = {
+      ExecStart = "${lib.getExe pkgs.prowlarr} -nobrowser -data=/mnt/media-config/prowlarr";
+    };
   };
 }
