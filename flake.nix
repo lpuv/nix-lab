ff5e52ab3c753d255637ffdf4b62c1fcda0d1ae4
Luna Puvilland <luna@craftcat.dev>
2024-11-01 13:53:43 -0700

    chore: update flake lock

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
 
     # lix (https://lix.systems)
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-1.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
