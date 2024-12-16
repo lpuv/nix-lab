cb7aeb2645a86692bb00c97f021751f200958b6e
Luna Puvilland <luna@craftcat.dev>
2024-12-15 17:51:10 -0800

    allow dotnet build

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -73,4 +73,11 @@
     users.root.openssh.authorizedKeys.keys = (import ./keys.nix).leo;
   };
 
+  nixpkgs.config.permittedInsecurePackages = [
+    "aspnetcore-runtime-6.0.36"
+    "aspnetcore-runtime-wrapped-6.0.36"
+    "dotnet-sdk-6.0.428"
+    "dotnet-sdk-wrapped-6.0.428"
+  ];
+
 }
