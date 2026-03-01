7f7b7a8f1741f5dce324265eac53749516c48b33
Luna Lâm Puvilland <luna@craftcat.dev>
2026-03-01 16:54:52 -0500

    bugfix(media): loosen even more :(

--- modules/services/media/radarr.nix
+++ modules/services/media/radarr.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }:
+{ lib, config, ... }:
 {
   services.radarr = {
     enable = true;
@@ -7,9 +7,19 @@
   };
 
   systemd.services.radarr.serviceConfig = {
-    SystemCallFilter = [ "@system-service" ];
+    # Revert #483483
+    # LXC handles the isolation; systemd trying to do it again breaks .NET for some reason
+    PrivateDevices = lib.mkForce false;
+    PrivateUsers = lib.mkForce false;
+    ProtectProc = lib.mkForce "default";
+    RestrictNamespaces = lib.mkForce false;
+  
+    # Allow .NET to access the host's entropy/randomness
+    SystemCallFilter = lib.mkForce [ ];
+  
+    # Required for .NET JIT in restricted environments
+    MemoryDenyWriteExecute = lib.mkForce false;
   };
-
 
   services.caddy.virtualHosts."radarr.media.internal.craftcat.dev" = {
     extraConfig = ''
--- modules/services/media/sonarr.nix
+++ modules/services/media/sonarr.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }:
+{ lib, config, ... }:
 {
   services.sonarr = {
     enable = true;
@@ -6,9 +6,20 @@
   };
 
   systemd.services.sonarr.serviceConfig = {
-    SystemCallFilter = [ "@system-service" ];
+    # Revert #483483
+    # LXC handles the isolation; systemd trying to do it again breaks .NET for some reason
+    PrivateDevices = lib.mkForce false;
+    PrivateUsers = lib.mkForce false;
+    ProtectProc = lib.mkForce "default";
+    RestrictNamespaces = lib.mkForce false;
+  
+    # Allow .NET to access the host's entropy/randomness
+    SystemCallFilter = lib.mkForce [ ];
+  
+    # Required for .NET JIT in restricted environments
+    MemoryDenyWriteExecute = lib.mkForce false;
   };
-
+  
   services.caddy.virtualHosts."sonarr.media.internal.craftcat.dev" = {
     extraConfig = ''
       import ${config.age.secrets.caddy.path}
