de1d096d01fa1eaeb69154f8826f7a2192009b18
Luna Lâm Puvilland <luna@craftcat.dev>
2026-08-22 19:13:54 -0400

    chore(caddy): update version of cloudflare

--- modules/services/media/caddy.nix
+++ modules/services/media/caddy.nix
@@ -4,8 +4,8 @@
   services.caddy = {
     enable = true;
     package = pkgs.caddy.withPlugins {
-      plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
-      hash = "sha256-7g8zDx5RhbptXFyEPtexxkHX8hw/gF001bZ7wX4Mjhs=";
+      plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
+      hash = "sha256-mqIa0wI/VfjDblg0NnkzKllWHXZZPLwHP8xEVSwZuPE=";
     };
 
     globalConfig = ''
